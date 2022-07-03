8bb488ba7b78338f84300d8cbc7e0e761f69a490
Loïc Callaerts <loic.callaerts@gmail.com>
2022-07-03 22:37:12 +0200

    utilisation de ${itms.smtng} dans script.js

---
+++ front/js/script.js
@@ -0,0 +1,18 @@
+fetch('http://localhost:3000/api/products/')
+.then(resultat => resultat.json())
+.then(Display => displayKanap(Display))
+.catch(error=> console.log(error))
+
+function displayKanap(kanaps){
+    const section = document.getElementById("items")
+    kanaps.forEach(items => {
+    section.innerHTML +=
+    `<a ${items.id}>
+    <article>
+      <img src = ${items.imageUrl} alt="Lorem ipsum dolor sit amet, Kanap name1">
+      <h3>  ${items.name}</h3>
+      <p> ${items.description}</p>
+    </article>
+  </a>`
+});
+}
